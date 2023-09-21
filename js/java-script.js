7e1e12bbb25c4d4e8098569cdc092cd68b8bdb62
franconuarte <franconuarte0@gmail.com>
2023-09-21 18:19:21 -0300

    agrego obj, arrays y funciones a js

--- js/java-script.js
+++ js/java-script.js
@@ -1,47 +1,155 @@
-let usuario = prompt("Ingresa un usuario");
+let menu = prompt("Que desea hacer?\n1- Crear cuenta\n2- Ver productos\n3- Ingresar a la pagina")
 
-while (usuario == "") {
-
-    if (usuario == "") {
-        alert("No ingresaste un usuario")
+while (menu > "3") {
+    if (menu > "3") {
+        alert("Numero del 1 al 3")
     }
-    usuario = prompt("Ingresa un usuario");
+    menu = prompt("Que desea hacer?\n1- Crear cuenta\n2- Ver productos\n3- Ingresar a la pagina")
 }
-alert("Usuario guardado con exito")
-
-let contraseña = prompt("Ingresa una contraseña");
-
-while (contraseña == "") {
-
-    if (contraseña == "") {
-        alert("No ingresaste una contraseña")
+while (menu < "1") {
+    if (menu < "1") {
+        alert("Numero del 1 al 3")
     }
-    contraseña = prompt("Ingresa una contraseña")
+    menu = prompt("Que desea hacer?\n1- Crear cuenta\n2- Ver productos\n3- Ingresar a la pagina")
 }
-alert("Contraseña guardada con exito")
 
 
-let confirmarus = prompt("Confirma tu usuario");
+if (menu == "1") {
+    let usuario = prompt("Ingresa un usuario");
 
+    while (usuario == "") {
 
-function confirmar(confirmarus, usuario) {
-    while (confirmarus != usuario) {
-        if (confirmarus != usuario) {
-            alert("Incorrecto")
+        if (usuario == "") {
+            alert("No ingresaste un usuario")
         }
-        confirmarus = prompt("Confirmar");
+        usuario = prompt("Ingresa un usuario");
     }
-    alert("Confirmado")
+    alert("Usuario guardado con exito")
 
+
+    let contraseña = prompt("Ingresa una contraseña");
+
+    while (contraseña == "") {
+
+        if (contraseña == "") {
+            alert("No ingresaste una contraseña")
+        }
+        contraseña = prompt("Ingresa una contraseña")
+    }
+    alert("Contraseña guardada con exito")
+
+
+
+
+    let confirmarus = prompt("Confirma tu usuario");
+
+
+    function confirmar(confirmarus, usuario) {
+        while (confirmarus != usuario) {
+            if (confirmarus != usuario) {
+                alert("Incorrecto")
+            }
+            confirmarus = prompt("Confirmar");
+        }
+        alert("Confirmado")
+
+
+    }
+
+    confirmar(confirmarus, usuario)
+
+
+    let confirmarco = prompt("Confirma tu contraseña");
+
+    confirmar(confirmarco, contraseña)
+
+    alert("Bienvenido " + usuario)
+}
+
+
+const productos = {
+    prod1: "1- PlayStation 5", precio1: "552.999",
+    prod2: "2- Dualshock", precio2: "67.326",
+    prod3: "3- Auriculares", precio3: "54.999",
+    prod4: "4- VR", precio4: "979.999",
+}
+const iva = [productos.precio1, productos.precio2, productos.precio3, productos.precio4]
+const mathe = {
+
+    PrecioConIva: 1.21
 
 }
 
-confirmar(confirmarus, usuario)
+
+if (menu == "2") {
 
 
-let confirmarco = prompt("Confirma tu contraseña");
+    const producto = ["1- PlayStation 5", "2- Dualshock", "3- Auriculares", "4- VR"];
 
-confirmar(confirmarco, contraseña)
+    alert(producto.join(" \n "))
+    const produc = prompt("Escriba el numero del producto para comprar")
 
-alert("Bienvenido " + usuario)
+    if (produc == "1") {
+        alert(producto.splice(0, 1))
+        const siono = prompt("¿Desea comprar este producto" + "\n" +
+            "si o no")
+        if (siono == "si") {
+            alert("El total es" + " " + "$" + productos.precio1 + " y con IVA " + "$" + mathe.PrecioConIva * productos.precio1 + "\n" +
+                "Gracias por su compra")
+        }
+        if (siono == "no") {
+            alert("Saliendo del menu de compras")
+        }
+
+    }
+    if (produc == "2") {
+        alert(producto.splice(1, 1))
+        const siono = prompt("¿Desea comprar este producto" + "\n" +
+            "si o no")
+        if (siono == "si") {
+            alert("El total es" + " " + "$" + productos.precio2 + " y con IVA " + "$" + mathe.PrecioConIva * productos.precio2 + "\n" +
+                "Gracias por su compra")
+        }
+        if (siono == "no") {
+            alert("Saliendo del menu de compras")
+        }
+
+    }
+    if (produc == "3") {
+        alert(producto.splice(2, 1))
+        const siono = prompt("¿Desea comprar este producto" + "\n" +
+            "si o no")
+        if (siono == "si") {
+            alert("El total es" + " " + "$" + productos.precio3 + " y con IVA " + "$" + mathe.PrecioConIva * productos.precio3 + "\n" +
+                "Gracias por su compra")
+        }
+        if (siono == "no") {
+            alert("Saliendo del menu de compras")
+        }
+
+    }
+    if (produc == "4") {
+        alert(producto.splice(3, 1))
+        const siono = prompt("¿Desea comprar este producto" + "\n" +
+            "Si o No")
+        if (siono == "si") {
+            alert("El total es" + " " + "$" + productos.precio4 + " y con IVA " + "$" + mathe.PrecioConIva * productos.precio4 + "\n" +
+                "Gracias por su compra")
+        }
+        if (siono == "no") {
+            alert("Saliendo del menu de compras")
+        }
+
+    }
+
+    if (prod != "1") {
+        alert("Bienvenido!!")
+    }
+
+}
+
+if (menu == "3") {
+    alert("Bienvenido!!")
+}
+
 
