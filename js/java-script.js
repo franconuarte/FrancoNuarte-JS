03e901ad042b38a4ebc21dc41fd0f7578a4b1746
franconuarte <franconuarte0@gmail.com>
2023-10-13 18:20:57 -0300

    agrego storage

--- js/java-script.js
+++ js/java-script.js
@@ -1,8 +1,8 @@
 const productos = {
-    prod1: "1- PlayStation 5", precio1: "552.999",
-    prod2: "2- Dualshock", precio2: "67.326",
-    prod3: "3- Auriculares", precio3: "54.999",
-    prod4: "4- VR", precio4: "979.999",
+    prod1: "- PlayStation 5", precio1: " $552.999",
+    prod2: "- Dualsense Edge", precio2: " $67.326",
+    prod3: "- Auriculares Pulse 3D", precio3: " $54.999",
+    prod4: "- Playstation VR2", precio4: " $979.999",
 }
 const iva = [productos.precio1, productos.precio2, productos.precio3, productos.precio4]
 const mathe = {
@@ -14,36 +14,60 @@ const mathe = {
 
 
 
+
 let compra1 = document.getElementById("comprar1")
-compra1.onclick = () => {
+compra1.addEventListener("click", function () {
     let comprado = document.getElementById("ps5")
-    comprado.innerHTML = "Agotado"
-    comprado.className = "red"
-    alert("El total es " + "$" + productos.precio1 + " y con IVA " + "$" + mathe.PrecioConIva * productos.precio1 + "\n" +
-        "Gracias por su compra")
-}
+    comprado.innerHTML = "Agregado al carrito"
+    comprado.className = "agregado"
+    let elim1 = document.getElementById("comprar1")
+    elim1.remove()
+    arr.push(productos.prod1 + productos.precio1)
+})
 let compra2 = document.getElementById("comprar2")
-compra2.onclick = () => {
+compra2.addEventListener("click", function () {
     let comprado = document.getElementById("dual")
-    comprado.innerHTML = "Agotado"
-    comprado.className = "red"
-    alert("El total es" + " " + "$" + productos.precio2 + " y con IVA " + "$" + mathe.PrecioConIva * productos.precio2 + "\n" +
-        "Gracias por su compra")
-}
+    comprado.innerHTML = "Agregado al carrito"
+    comprado.className = "agregado"
+    let elim2 = document.getElementById("comprar2")
+    elim2.remove()
+    arr.push(productos.prod2 + productos.precio2)
+})
 let compra3 = document.getElementById("comprar3")
-compra3.onclick = () => {
+compra3.addEventListener("click", function () {
     let comprado = document.getElementById("auri")
-    comprado.innerHTML = "Agotado"
-    comprado.className = "red"
-    alert("El total es" + " " + "$" + productos.precio3 + " y con IVA " + "$" + mathe.PrecioConIva * productos.precio3 + "\n" +
-        "Gracias por su compra")
-}
+    comprado.innerHTML = "Agregado al carrito"
+    comprado.className = "agregado"
+    let elim3 = document.getElementById("comprar3")
+    elim3.remove()
+    arr.push(productos.prod3 + productos.precio3)
+})
 let compra4 = document.getElementById("comprar4")
-compra4.onclick = () => {
+compra4.addEventListener("click", function () {
     let comprado = document.getElementById("psvr")
-    comprado.innerHTML = "Agotado"
-    comprado.className = "red"
-    alert("El total es" + " " + "$" + productos.precio4 + " y con IVA " + "$" + mathe.PrecioConIva * productos.precio4 + "\n" +
-        "Gracias por su compra")
-}
+    comprado.innerHTML = "Agregado al carrito"
+    comprado.className = "agregado"
+    let elim4 = document.getElementById("comprar4")
+    elim4.remove()
+    arr.push(productos.prod4 + productos.precio4)
+})
+
+const arr = []
+
+
+
+let btn = document.getElementById("btn")
+btn.addEventListener("click", function () {
+    let carri = document.getElementById("data")
+    carri.innerHTML = arr
+    localStorage.setItem("carrito", arr)
+})
+
+let btn2 = document.getElementById("btn2")
+btn2.addEventListener("click", function () {
+    let carri = document.getElementById("data")
+    carri.innerHTML = localStorage.getItem("carrito")
+    
+})
+
 
